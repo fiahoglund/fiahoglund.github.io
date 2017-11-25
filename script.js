ec91ac092974fa1f30f3634c7c79e78dc3192bdd
marie-hoglund <maarie.hoglund@gmail.com>
2017-11-25 12:27:04 +0100

    wip

--- script.js
+++ script.js
@@ -1,17 +1,18 @@
 
 
 function myFunction() {
-    if( document.getElementById("imgClickAndChange").src== "http://fiahoglund.github.io/Seed.svg")
+    if( document.getElementById("imgClickAndChange").src== "https://fiahoglund.github.io/Seed.svg")
     {
       //ändra bilden till trädet
       document.getElementById("imgClickAndChange").src = "leaf.png";
 
-
       //länka till html sidan där man sätter upp sitt konto
+      window.open("pages/setUpPage");
 
     }
     else
     {
       //Länka till html sidan för att kolla på kontot man redan har
+      window.open("pages/checkOutTree");
     }
 }
