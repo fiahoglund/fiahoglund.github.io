e4ceb2c60d5ef1c1e37b3f2e628712dad4e077b9
marie-hoglund <maarie.hoglund@gmail.com>
2017-11-25 11:33:01 +0100

    wq

---
+++ script.js
@@ -0,0 +1,17 @@
+
+
+function myFunction() {
+    if( document.getElementById("imgClickAndChange").src== "file:///D:/Marie/work/casoma_grow/fiahoglund.github.io/Seed.svg")
+    {
+      //ändra bilden till trädet
+      document.getElementById("imgClickAndChange").src = "leaf.png";
+
+
+      //länka till html sidan där man sätter upp sitt konto
+
+    }
+    else
+    {
+      //Länka till html sidan för att kolla på kontot man redan har
+    }
+}
