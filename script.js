96b7cb9ec85cd7d6393f9d1ccdb1e0b813672888
marie-hoglund <maarie.hoglund@gmail.com>
2017-11-25 11:39:28 +0100

    wip

--- script.js
+++ script.js
@@ -1,7 +1,7 @@
 
 
 function myFunction() {
-    if( document.getElementById("imgClickAndChange").src== "file:///D:/Marie/work/casoma_grow/fiahoglund.github.io/Seed.svg")
+    if( document.getElementById("imgClickAndChange").src== "http://fiahoglund.github.io/Seed.svg")
     {
       //ändra bilden till trädet
       document.getElementById("imgClickAndChange").src = "leaf.png";
